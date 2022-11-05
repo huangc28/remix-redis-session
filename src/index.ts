2f3c2a9324f014c478dac3e5e01e1468e621f259
Chi-Han Huang <huangchihan@gmail.com>
2022-11-05 15:50:13 +0800

    Minor refactor.

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,504 @@
-import { SessionIdStorageStrategy, SessionStorage, createSessionStorageFactory } from "@remix-run/server-runtime";
-import { createCookie } from "@remix-run/node";
+import type { SessionIdStorageStrategy, SessionStorage } from "@remix-run/server-runtime";
+import { createSessionStorage } from '@remix-run/node';
 
+import type { RedisOptions, Redis } from 'ioredis';
 
-import { randomBytes as crypto_randomBytes } from "crypto";
+import crypto from "crypto";
+import IORedis from "ioredis";
 
-import Redis from "ioredis";
 function genRandomID(): string {
-    const randomBytes = crypto_randomBytes(8);
-    return Buffer.from(randomBytes).toString("hex");
+  const randomBytes = crypto.randomBytes(8);
+  return Buffer.from(randomBytes).toString("hex");
 }
 
 const expiresToSeconds = (expires: Date) => {
-    const now = new Date();
-    const expiresDate = new Date(expires);
-    const secondsDelta = (expiresDate.getTime() - now.getTime())/1000;
-    return secondsDelta < 0 ? 0 : secondsDelta;
+  const now = new Date();
+  const expiresDate = new Date(expires);
+  const secondsDelta = (expiresDate.getTime() - now.getTime()) / 1000;
+  return secondsDelta < 0 ? 0 : secondsDelta;
 };
 
 type redisSessionArguments = {
-    cookie: SessionIdStorageStrategy["cookie"];
-    options: {
-        redisConfig?: Redis.RedisOptions,
-        redisClient?: Redis.Redis
-    }
+  cookie: SessionIdStorageStrategy["cookie"];
+  options: {
+    redisConfig?: RedisOptions,
+    redisClient?: Redis
+  }
 };
 
 export function createRedisSessionStorage({
-    cookie,
-    options
+  cookie,
+  options
 }: redisSessionArguments): SessionStorage {
-    let redis: Redis.Redis
-    if (options.redisClient) {
-        redis = options.redisClient
-    } else if (options.redisConfig) {
-        redis = new Redis(options.redisConfig);
-    } else {
-        throw new Error("Need to provide either options.RedisConfig or options.redisClient")
-    }
-    const createSessionStorage = createSessionStorageFactory(createCookie)
-    return createSessionStorage({
-        cookie,
-        async createData(data, expires) {
-            const id = genRandomID();
-            if (expires) {
-                await redis.set(
-                    id,
-                    JSON.stringify(data),
-                    "EX",
-                    expiresToSeconds(expires)
-                );
-            } else {
-                await redis.set(id, JSON.stringify(data));
-            }
-            return id;
-        },
-        async readData(id) {
-            const data = await redis.get(id);
-            if (data) {
-                return JSON.parse(data);
-            }
-            return null;
-        },
-        async updateData(id, data, expires) {
-            if (expires) {
-                await redis.set(
-                    id,
-                    JSON.stringify(data),
-                    "EX",
-                    expiresToSeconds(expires)
-                );
-            } else {
-                await redis.set(id, JSON.stringify(data));
-            }
-        },
-        async deleteData(id) {
-            await redis.del(id);
-        },
-    });
+  let redis: Redis
+  if (options.redisClient) {
+    redis = options.redisClient
+  } else if (options.redisConfig) {
+    redis = new IORedis(options.redisConfig);
+  } else {
+    throw new Error("Need to provide either options.RedisConfig or options.redisClient")
+  }
+
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      const id = genRandomID();
+      console.log('debug redis create data', id);
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+      return id;
+    },
+    async readData(id) {
+      const data = await redis.get(id);
+      if (data) {
+        return JSON.parse(data);
+      }
+      return null;
+    },
+    async updateData(id, data, expires) {
+      console.log('debug redis updateData', id);
+
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+    },
+    async deleteData(id) {
+      await redis.del(id);
+    },
+  });
+}
+import type { SessionIdStorageStrategy, SessionStorage } from "@remix-run/server-runtime";
+import { createSessionStorage } from '@remix-run/node';
+
+import type { RedisOptions, Redis } from 'ioredis';
+
+import crypto from "crypto";
+import IORedis from "ioredis";
+
+function genRandomID(): string {
+  const randomBytes = crypto.randomBytes(8);
+  return Buffer.from(randomBytes).toString("hex");
+}
+
+const expiresToSeconds = (expires: Date) => {
+  const now = new Date();
+  const expiresDate = new Date(expires);
+  const secondsDelta = (expiresDate.getTime() - now.getTime()) / 1000;
+  return secondsDelta < 0 ? 0 : secondsDelta;
+};
+
+type redisSessionArguments = {
+  cookie: SessionIdStorageStrategy["cookie"];
+  options: {
+    redisConfig?: RedisOptions,
+    redisClient?: Redis
+  }
+};
+
+export function createRedisSessionStorage({
+  cookie,
+  options
+}: redisSessionArguments): SessionStorage {
+  let redis: Redis
+  if (options.redisClient) {
+    redis = options.redisClient
+  } else if (options.redisConfig) {
+    redis = new IORedis(options.redisConfig);
+  } else {
+    throw new Error("Need to provide either options.RedisConfig or options.redisClient")
+  }
+
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      const id = genRandomID();
+      console.log('debug redis create data', id);
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+      return id;
+    },
+    async readData(id) {
+      const data = await redis.get(id);
+      if (data) {
+        return JSON.parse(data);
+      }
+      return null;
+    },
+    async updateData(id, data, expires) {
+      console.log('debug redis updateData', id);
+
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+    },
+    async deleteData(id) {
+      await redis.del(id);
+    },
+  });
+}
+import type { SessionIdStorageStrategy, SessionStorage } from "@remix-run/server-runtime";
+import { createSessionStorage } from '@remix-run/node';
+
+import type { RedisOptions, Redis } from 'ioredis';
+
+import crypto from "crypto";
+import IORedis from "ioredis";
+
+function genRandomID(): string {
+  const randomBytes = crypto.randomBytes(8);
+  return Buffer.from(randomBytes).toString("hex");
+}
+
+const expiresToSeconds = (expires: Date) => {
+  const now = new Date();
+  const expiresDate = new Date(expires);
+  const secondsDelta = (expiresDate.getTime() - now.getTime()) / 1000;
+  return secondsDelta < 0 ? 0 : secondsDelta;
+};
+
+type redisSessionArguments = {
+  cookie: SessionIdStorageStrategy["cookie"];
+  options: {
+    redisConfig?: RedisOptions,
+    redisClient?: Redis
+  }
+};
+
+export function createRedisSessionStorage({
+  cookie,
+  options
+}: redisSessionArguments): SessionStorage {
+  let redis: Redis
+  if (options.redisClient) {
+    redis = options.redisClient
+  } else if (options.redisConfig) {
+    redis = new IORedis(options.redisConfig);
+  } else {
+    throw new Error("Need to provide either options.RedisConfig or options.redisClient")
+  }
+
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      const id = genRandomID();
+      console.log('debug redis create data', id);
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+      return id;
+    },
+    async readData(id) {
+      const data = await redis.get(id);
+      if (data) {
+        return JSON.parse(data);
+      }
+      return null;
+    },
+    async updateData(id, data, expires) {
+      console.log('debug redis updateData', id);
+
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+    },
+    async deleteData(id) {
+      await redis.del(id);
+    },
+  });
+}
+import type { SessionIdStorageStrategy, SessionStorage } from "@remix-run/server-runtime";
+import { createSessionStorage } from '@remix-run/node';
+
+import type { RedisOptions, Redis } from 'ioredis';
+
+import crypto from "crypto";
+import IORedis from "ioredis";
+
+function genRandomID(): string {
+  const randomBytes = crypto.randomBytes(8);
+  return Buffer.from(randomBytes).toString("hex");
+}
+
+const expiresToSeconds = (expires: Date) => {
+  const now = new Date();
+  const expiresDate = new Date(expires);
+  const secondsDelta = (expiresDate.getTime() - now.getTime()) / 1000;
+  return secondsDelta < 0 ? 0 : secondsDelta;
+};
+
+type redisSessionArguments = {
+  cookie: SessionIdStorageStrategy["cookie"];
+  options: {
+    redisConfig?: RedisOptions,
+    redisClient?: Redis
+  }
+};
+
+export function createRedisSessionStorage({
+  cookie,
+  options
+}: redisSessionArguments): SessionStorage {
+  let redis: Redis
+  if (options.redisClient) {
+    redis = options.redisClient
+  } else if (options.redisConfig) {
+    redis = new IORedis(options.redisConfig);
+  } else {
+    throw new Error("Need to provide either options.RedisConfig or options.redisClient")
+  }
+
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      const id = genRandomID();
+      console.log('debug redis create data', id);
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+      return id;
+    },
+    async readData(id) {
+      const data = await redis.get(id);
+      if (data) {
+        return JSON.parse(data);
+      }
+      return null;
+    },
+    async updateData(id, data, expires) {
+      console.log('debug redis updateData', id);
+
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+    },
+    async deleteData(id) {
+      await redis.del(id);
+    },
+  });
+}
+import type { SessionIdStorageStrategy, SessionStorage } from "@remix-run/server-runtime";
+import { createSessionStorage } from '@remix-run/node';
+
+import type { RedisOptions, Redis } from 'ioredis';
+
+import crypto from "crypto";
+import IORedis from "ioredis";
+
+function genRandomID(): string {
+  const randomBytes = crypto.randomBytes(8);
+  return Buffer.from(randomBytes).toString("hex");
+}
+
+const expiresToSeconds = (expires: Date) => {
+  const now = new Date();
+  const expiresDate = new Date(expires);
+  const secondsDelta = (expiresDate.getTime() - now.getTime()) / 1000;
+  return secondsDelta < 0 ? 0 : secondsDelta;
+};
+
+type redisSessionArguments = {
+  cookie: SessionIdStorageStrategy["cookie"];
+  options: {
+    redisConfig?: RedisOptions,
+    redisClient?: Redis
+  }
+};
+
+export function createRedisSessionStorage({
+  cookie,
+  options
+}: redisSessionArguments): SessionStorage {
+  let redis: Redis
+  if (options.redisClient) {
+    redis = options.redisClient
+  } else if (options.redisConfig) {
+    redis = new IORedis(options.redisConfig);
+  } else {
+    throw new Error("Need to provide either options.RedisConfig or options.redisClient")
+  }
+
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      const id = genRandomID();
+      console.log('debug redis create data', id);
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+      return id;
+    },
+    async readData(id) {
+      const data = await redis.get(id);
+      if (data) {
+        return JSON.parse(data);
+      }
+      return null;
+    },
+    async updateData(id, data, expires) {
+      console.log('debug redis updateData', id);
+
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+    },
+    async deleteData(id) {
+      await redis.del(id);
+    },
+  });
+}
+import type { SessionIdStorageStrategy, SessionStorage } from "@remix-run/server-runtime";
+import { createSessionStorage } from '@remix-run/node';
+
+import type { RedisOptions, Redis } from 'ioredis';
+
+import crypto from "crypto";
+import IORedis from "ioredis";
+
+function genRandomID(): string {
+  const randomBytes = crypto.randomBytes(8);
+  return Buffer.from(randomBytes).toString("hex");
+}
+
+const expiresToSeconds = (expires: Date) => {
+  const now = new Date();
+  const expiresDate = new Date(expires);
+  const secondsDelta = (expiresDate.getTime() - now.getTime()) / 1000;
+  return secondsDelta < 0 ? 0 : secondsDelta;
+};
+
+type redisSessionArguments = {
+  cookie: SessionIdStorageStrategy["cookie"];
+  options: {
+    redisConfig?: RedisOptions,
+    redisClient?: Redis
+  }
+};
+
+export function createRedisSessionStorage({
+  cookie,
+  options
+}: redisSessionArguments): SessionStorage {
+  let redis: Redis
+  if (options.redisClient) {
+    redis = options.redisClient
+  } else if (options.redisConfig) {
+    redis = new IORedis(options.redisConfig);
+  } else {
+    throw new Error("Need to provide either options.RedisConfig or options.redisClient")
+  }
+
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      const id = genRandomID();
+      console.log('debug redis create data', id);
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+      return id;
+    },
+    async readData(id) {
+      const data = await redis.get(id);
+      if (data) {
+        return JSON.parse(data);
+      }
+      return null;
+    },
+    async updateData(id, data, expires) {
+      console.log('debug redis updateData', id);
+
+      if (expires) {
+        await redis.set(
+          id,
+          JSON.stringify(data),
+          "EX",
+          expiresToSeconds(expires)
+        );
+      } else {
+        await redis.set(id, JSON.stringify(data));
+      }
+    },
+    async deleteData(id) {
+      await redis.del(id);
+    },
+  });
 }
